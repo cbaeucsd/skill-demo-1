fbbc1a88472fde0314713902fb7bf0004383d444
cbaeucsd <cbae@ucsd.edu>
2022-04-28 00:35:45 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void max(){
-        assertEquals(1, SkillDemo.max(1,2));
+        assertEquals(2, SkillDemo.max(1,2));
     }
 }
