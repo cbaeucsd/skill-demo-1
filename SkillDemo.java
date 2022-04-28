4b5039e75aa67de121ee5718379c993a4f63ab20
cbaeucsd <cbae@ucsd.edu>
2022-04-28 00:34:21 -0700

    method + failed test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,2 +1,6 @@
 public class SkillDemo{
+    public static int max(int a, int b){
+        if (a > b) return a;
+        return b;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void max(){
+        assertEquals(1, SkillDemo.max(1,2));
+    }
+}
